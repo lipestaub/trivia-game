ff17038ed15110105fcd3018b8bd881c4c8ba87b
Rafazanella01 <rafazanella2004@gmail.com>
2024-02-05 15:05:14 -0300

    Add sql

--- trivia.sql
+++ trivia.sql
@@ -1,3 +1,38 @@
 CREATE DATABASE trivia
 
-CREATE TABLE
+CREATE TABLE public.user(
+    id serial4 NOT NULL,
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(32) NOT NULL,
+    CONSTRAINT user_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE public.question(
+    id serial4 NOT NULL,
+    type VARCHAR(10) NOT NULL,
+    difficulty VARCHAR(10) NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    text VARCHAR(255) NOT NULL,
+    correct_answer VARCHAR(255) NOT NULL,
+    incorrect_answers VARCHAR(255) NOT NULL,
+    CONSTRAINT question_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE public.game(
+    id serial4 NOT NULL,
+    user_id int4 NOT NULL,
+    start_date TIMESTAMP NOT NULL,
+    CONSTRAINT game_pkey PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+CREATE TABLE public.game_questions(
+    id serial4 NOT NULL,
+    game_id int4 NOT NULL,
+    question_id int4 NOT NULL,
+    user_answer VARCHAR(255) NOT NULL,
+    is_correct BOOLEAN NOT NULL,
+    CONSTRAINT game_questions_pkey PRIMARY KEY (id),
+    FOREIGN KEY (game_id) REFERENCES game(id),
+    FOREIGN KEY (question_id) REFERENCES question(id)
+);
