d37299fa907a1e4add578521e133b255ebdaebf5
Felipe <lipestaub@gmail.com>
2024-02-05 16:42:42 -0300

    add config

---
+++ config/Connect.php
@@ -0,0 +1,12 @@
+<?php
+    class Connection {
+        public static function getConnection() {
+            try {
+                return new PDO('pgsql:host=localhost;dbname=trivia', 'postgres', 'postgres');
+            }
+            catch (PDOException $exception) {
+                echo "Connection failed: " . $exception->getMessage();
+            }
+        }
+    }
+?>
